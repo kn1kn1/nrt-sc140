5bce887c085970813b0c1d178f3ee3da253b8272
kn1kn1 <k71kn1@gmail.com>
2012-01-10 20:25:17 +0900

    applied some code conventions

--- nrtsc140.js
+++ nrtsc140.js
@@ -14,13 +14,13 @@ var sio = require('socket.io')
   , child_process = require('child_process')
   , path = require('path')
   , crypto = require('crypto')
-  , util = require('util')
+  , util = require('util');
 
 var NrtSc140 = exports.NrtSc140 = function(socket) {
   this._socket = socket;
   this._audioDir = path.join(process.cwd(), 'public', 'audio');
   this._curFile = null;
-  this._generatedFiles = new Array();
+  this._generatedFiles = [];
   this._timeoutId = null;
   this._intervalId = null;
   this._sclang = null;
@@ -34,22 +34,26 @@ NrtSc140.prototype.start = function() {
   this._socket.on('stop', this.onStop.bind(this));
   this._socket.on('restartsclang', this.onRestartSclang.bind(this));
   this._socket.on('disconnect', this.onSocketDisconnect.bind(this));
-}
+};
 
 NrtSc140.prototype.onRestartSclang = function() {
   this._socket.emit('scserverstarting');
-  if (this._sclang) this._sclang.dispose();
+  if (this._sclang) {
+    this._sclang.dispose();
+  }
   this._sclang = this.createSclang();
-}
+};
 
 NrtSc140.prototype.onValidate = function(msg) {
   util.debug('validate: ' + msg);
   this.validateScCode(msg, this._socket);
-}
+};
 
 NrtSc140.prototype.onGenerate = function(msg) {
   util.debug('generate: ' + msg);
-  if (!this.validateScCode(msg, this._socket)) return;
+  if (!this.validateScCode(msg, this._socket)) {
+    return;
+  }
 
   var aiffFile;
   do {
@@ -59,9 +63,10 @@ NrtSc140.prototype.onGenerate = function(msg) {
   } while (path.existsSync(aiffFile));
   //this._generatedFiles.push(path.join(_audioDir, _curFile + '.*'));
   this._sclang.evaluate(
-    's.waitForBoot(s.prepareForRecord(\'' 
-    + aiffFile + '\');s.record;' 
-    + msg + '\n);', false);
+    's.waitForBoot(s.prepareForRecord(\'' +
+    aiffFile + '\');s.record;' +
+    msg + '\n);', 
+    false);
 
   var timeRemaining = MAX_RECORDING_SEC + 1;
   this._timeoutId = setTimeout(function() {
@@ -75,26 +80,38 @@ NrtSc140.prototype.onGenerate = function(msg) {
     timeRecorded++;
     util.debug('timerecorded: ' + timeRecorded);
     this._socket.emit('timerecorded', '' + timeRecorded);
-    if (timeRecorded >= MAX_RECORDING_SEC) clearInterval(this._intervalId);
+    if (timeRecorded >= MAX_RECORDING_SEC) {
+      clearInterval(this._intervalId);
+    }
   }.bind(this), 1000);
 };
 
 NrtSc140.prototype.onStop = function() {
   util.debug('stop');
-  if (this._timeoutId) clearTimeout(this._timeoutId);
-  if (this._intervalId) clearInterval(this._intervalId);
-  this._timeoutId = this.stopRecording();
-}
+  if (this._timeoutId) {
+    clearTimeout(this._timeoutId);
+  }
+  if (this._intervalId) {
+    clearInterval(this._intervalId);
+  }
+  this.stopRecording();
+};
 
 NrtSc140.prototype.onSocketDisconnect = function() {
   util.debug('disconnect');
-  if (this._timeoutId) clearTimeout(this._timeoutId);
-  if (this._intervalId) clearInterval(this._intervalId);
-  if (this._sclang) this._sclang.dispose();
-  for (var i in this._generatedFiles) {
+  if (this._timeoutId) {
+    clearTimeout(this._timeoutId);
+  }
+  if (this._intervalId) {
+    clearInterval(this._intervalId);
+  }
+  if (this._sclang) {
+    this._sclang.dispose();
+  }
+  for (var i = 0, len = this._generatedFiles.length; i < len; i++) {
     rmFile(this._generatedFiles[i]);
   }
-}
+};
 
 NrtSc140.prototype.createSclang = function() {
   var sclang = 
@@ -108,12 +125,12 @@ NrtSc140.prototype.createSclang = function() {
   } while (path.existsSync(workaroundTmpFile));
   this._generatedFiles.push(workaroundTmpFile);
   sclang.evaluate(
-    's.waitForBoot(s.prepareForRecord(\'' 
-    + workaroundTmpFile
-    + '\');s.record;s.stopRecording;);', 
+    's.waitForBoot(s.prepareForRecord(\'' +
+    workaroundTmpFile +
+    '\');s.record;s.stopRecording;);', 
     false);
   return sclang;
-}
+};
 
 NrtSc140.prototype.onSclangStdoutReceived = function(data) {
   util.debug('sclang stdout: ' + data);
@@ -127,7 +144,7 @@ NrtSc140.prototype.onSclangStdoutReceived = function(data) {
     return; // ignore
   }
   this._socket.emit('stdout', msg);
-}
+};
 
 NrtSc140.prototype.validateScCode = function(msg) {
   if (msg.length < 1) {
@@ -144,7 +161,7 @@ NrtSc140.prototype.validateScCode = function(msg) {
   }
   this._socket.emit('validationerror', '');
   return true;
-}
+};
 
 NrtSc140.prototype.stopRecording = function() {
   this._sclang.evaluate('s.stopRecording;', false);
@@ -155,13 +172,13 @@ NrtSc140.prototype.stopRecording = function() {
     convert(aiffFile, mp3File, function(error, stdout, stderr) {
       util.debug('stdout: ' + stdout);
       util.debug('stderr: ' + stderr);
-      if (error != null) {
+      if (error) {
         util.debug('exec error: ' + error);
       }
       this._socket.emit('filegenerated', this._curFile);
     }.bind(this));
   }.bind(this), 1000);
-}
+};
 
 function randomString() {
   var buf = crypto.randomBytes(4);
@@ -174,8 +191,8 @@ function randomString() {
 
 function convert(aiffFile, mp3File, callback) {
   child_process.exec(
-    'ffmpeg -i ' + aiffFile + ' -f mp3 -acodec libmp3lame -ab 192000 -ar 44100 ' 
-    + mp3File, 
+    'ffmpeg -i ' + aiffFile + 
+    ' -f mp3 -acodec libmp3lame -ab 192000 -ar 44100 ' + mp3File, 
     callback);
 }
 
@@ -183,7 +200,7 @@ function rmFile(file) {
   child_process.exec('rm -f ' + file, function(error, stdout, stderr) {
     util.debug('stdout: ' + stdout);
     util.debug('stderr: ' + stderr);
-    if (error != null) {
+    if (error) {
       util.debug('exec error: ' + error);
     }
   });
